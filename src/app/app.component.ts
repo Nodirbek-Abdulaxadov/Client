382693092852166b3e05e02b00b71e9e55d29a4a
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2024-07-31 08:41:59 +0500

    Initialize Repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 import { RouterOutlet } from '@angular/router';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    Button
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Client';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
